94178bc9799e4620a7acb645be229169f6810be5
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-02-21 17:26:06 +0530

    Update Java1.java Again

--- Java1.java
+++ Java1.java
@@ -4,10 +4,11 @@ class Java1
         System.out.println("Hello world");
         System.out.println("Hello Kunal");
         System.out.println("Hello Sujit");
-       
+       System.out.println("Hello Ghogale's");
     }
 
 }
+
 
 
 
