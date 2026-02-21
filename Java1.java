73850e99f1ac6a0f26375767e7bb8757eb775577
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-02-21 13:01:55 +0530

    Update Java1.java second time

--- Java1.java
+++ Java1.java
@@ -3,6 +3,8 @@ class Java1
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello Kunal");
+        System.out.println("Hello Sujit");
     }
 
 }
+
