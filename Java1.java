ec881c801016186ad3ad21a229cfec49985303c5
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-02-21 17:57:47 +0530

    Update Java1.java arithmatic operations

--- Java1.java
+++ Java1.java
@@ -3,14 +3,16 @@ class Java1
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello Kunal");
-        System.out.println("Hello Sujit");
-       System.out.println("Hello Ghogale's");
-        System.out.println("Hello Collegue's Now i also learn hoe to trigger jobs in jenkins using git");
-        System.out.println("Hello Dell");
-        System.out.println("Today is weekend");
+        int no1=100;
+        int no2=200;
+        System.out.println("Addition:"+(no1+no2));
+        System.out.println("Substraction:"+(no1-no2));
+        System.out.println("Multiplication:"+(no1*no2));
+        System.out.println("Division:"+(no1/no2));
     }
 
 }
+
 
 
 
