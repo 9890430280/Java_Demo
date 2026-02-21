7b78ce2d84d5abd2a8490e440b38c0b87b845923
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-02-21 18:27:09 +0530

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -1,24 +1,6 @@
-class Java1
-{
+public class JavaApp {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello Kunal");
-        int no1=100;
-        int no2=200;
-        System.out.println("Addition:"+(no1+no2));
-        System.out.println("Substraction:"+(no1-no2));
-        System.out.println("Multiplication:"+(no1*no2));
-        System.out.println("Division:"+(no1/no2));
+        System.out.println("Jenkins CI Pipeline Demo");
+        System.out.println("Build Successful!");
     }
-
 }
-
-
-
-
-
-
-
-
-
-
