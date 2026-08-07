493685b2ef1c0fd39253ab8d237470cac12cf61d
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-08-07 17:23:37 +0530

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -1,10 +1,7 @@
 public class Java1 {
     public static void main(String[] args) {
         System.out.println("Hello Kunal!");
-        System.out.println("Jenkins CI Pipeline Demo");
-        System.out.println("Build Successful!");
-        System.out.println("Executed Successful!");
-        System.out.println("Hello Again, After along time");
+        
     }
 }
 
