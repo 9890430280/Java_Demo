5ba394dae47ea23d1b570195c96cac9b5aa9eb6a
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-08-22 13:14:48 +0530

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -3,6 +3,9 @@ public class Java1 {
         System.out.println("Hello Kunal!");
         System.out.println("Now Jenkins succesfully pull the code from github and run job in jenkins in every two minutes!!!!");
         System.out.println("I just created new pipeline for this repo.");
+        System.out.println("Hello Kunal!");
+        System.out.println("Now Jenkins succesfully pull the code from github and run job in jenkins in every two minutes!!!!");
+        System.out.println("I just created new pipeline for this repo.");
         
         
         
