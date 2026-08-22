4793cd0a62a16f66ef05e57daa13834c9fe9f9a3
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-08-22 12:56:19 +0530

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -2,6 +2,7 @@ public class Java1 {
     public static void main(String[] args) {
         System.out.println("Hello Kunal!");
         System.out.println("Now Jenkins succesfully pull the code from github and run job in jenkins in every two minutes!!!!");
+        System.out.println("Hello Kunal, Welcome back to build our future");
         
         
         
